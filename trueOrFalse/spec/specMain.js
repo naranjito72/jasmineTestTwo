09e2295e79406ba5aff00d418e4b77c13754902d
naranjito72 <theclick36@gmail.com>
2021-06-11 13:43:15 +0200

    test done

--- trueOrFalse/spec/specMain.js
+++ trueOrFalse/spec/specMain.js
@@ -49,5 +49,52 @@ describe('Returning "undefined"', function(){
     let obj = {}
     expect(thereAreDuplicates(obj)).not.toBe(null);
     expect(thereAreDuplicates(obj)).toBe(undefined);
-  })
-})
+  });
+});
+/****************************************hasDuplicates()***************************************************/
+describe('hasDuplicates', function(){
+  it('Describes hasDuplicates as a function', function(){
+    expect(typeof(hasDuplicates)).toBe('function');
+  });
+
+  it('The function returns a boolean that can be false', function(){
+    let matches = false;
+    expect(matches).toBe(false);
+  });
+
+  it('The function returns a boolean that can be true', function(){
+    let matches = true;
+    expect(matches).not.toBe(false);
+  });
+});
+describe('When to return false', function(){
+  const arrParameters = [
+    {case: hasDuplicates([1, 2, 3, 4]), answer: false},
+    {case: hasDuplicates([5, 6, 7, 8]), answer: false},
+    {case: hasDuplicates([9, 10, 11, 12]), answer: false}
+  ]
+  arrParameters.forEach((item, i) => {
+    it(`Should return "false" when no repeated matches array number: ${i + 1} return: ${item.answer} `, function(){
+      expect(item.case).toEqual(item.answer)
+    })
+  });
+});
+
+describe('When to return true', function(){
+  const arrParameters = [
+    {case: hasDuplicates([1, 2, 3, 4, 1]), answer: true},
+    {case: hasDuplicates([5, 6, 7, 8, 6, 7]), answer: true},
+    {case: hasDuplicates([9, 10, 11, 12, 9, 10, 11]), answer: true}
+  ]
+  arrParameters.forEach((item, i) => {
+    it(`Should return "true" when repeated matches are inside the array number: ${i + 1} return: ${item.answer} `, function(){
+      expect(item.case).toEqual(item.answer)
+    })
+  });
+});
+describe('Returning "false"', function(){
+  it('Should return "false" when is injected an empty array as a parameter', function(){
+    let arr = [];
+    expect(hasDuplicates(arr)).toEqual(false);
+  });
+});
